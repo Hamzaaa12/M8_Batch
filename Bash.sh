4ba546527d568136f49cc4bde9e1d569868256a7
Hamzaaa12 <77388229+Hamzaaa12@users.noreply.github.com>
2021-06-07 10:54:07 +0200

    Add files via upload

---
+++ Bash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /home/b8/Escritorio
+mkdir Asignaturas
+cd Asignaturas
+mkdir Ingles Catalan Castellano Matematicas Historia Fisica Quimica Educacion_Fisica Otras
+sudo chmod 764 Ingles Catalan Castellano Matematicas Historia Fisica Quimica Educacion_Fisica Otras
